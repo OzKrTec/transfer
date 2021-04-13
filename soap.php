1bc8ca6e23fd683924764a200bf478c7d3c9a0ab
OzKrTec <73920611+OzKrTec@users.noreply.github.com>
2021-04-13 16:27:49 -0500

    Update soap.php

--- soap.php
+++ soap.php
@@ -60,7 +60,7 @@ $options = array(
     'referenceNumber3' => 'desc',
     'entity_url' => 'https://pruebas.enercer.com/main',
     'email' => 'demo@gmail.com',
-    'id_cliente' => '91541579',
+     'fields'=>array ('id_cliente'=>'91541579'),
 
 );
     $url = 'https://200.1.124.65/PSEHostingWebServices/PSEHostingWS.asmx?WSDL';
