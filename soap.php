0c989d7e7638e21ac4d925dd75fdb9a97a96c91c
OzKrTec <73920611+OzKrTec@users.noreply.github.com>
2021-04-23 00:04:53 -0500

    Update soap.php

--- soap.php
+++ soap.php
@@ -92,6 +92,7 @@ $options = array(
      $result = $client->createTransactionPaymentHosting($options);//llamamos al métdo que nos interesa con los parámetros
 
     var_dump($result);
+    var_dump($result['PaymentIdentifier']);
 
     // $value = get_object_vars($result);
 
