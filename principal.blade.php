3ee4d52e3815409b3c7aaa57fc96f7f506b9d00b
OzKrTec <73920611+OzKrTec@users.noreply.github.com>
2021-07-02 00:26:01 -0500

    Update principal.blade.php

--- principal.blade.php
+++ principal.blade.php
@@ -23,31 +23,13 @@
         <link href="css/plantilla.css" rel="stylesheet">
 
         {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" /> --}}
-        <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet"/>
+        {{-- <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet"/> --}}
     </head>
 
     <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
     <div id="app">
     <header class="app-header navbar">
-       <button   class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#"></a>
-        <button id="close-sidebar" class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <button @click="menu=57"class="" type="button">
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <ul class="nav navbar-nav d-md-down-none">
-            <li class="nav-item px-3">
-                <a class="nav-link" href="#">Escritorio</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link" href="#">Configuraciones</a>
-            </li>
-        </ul>
         <ul class="nav navbar-nav ml-auto">
             <li class="nav-item d-md-down-none">
                 <a class="nav-link" href="#" data-toggle="dropdown">
